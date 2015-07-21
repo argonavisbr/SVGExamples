4825df1d53570dc5395685d25140295330bb5e4d
helderdarocha <helder.darocha@gmail.com>
2015-07-21 02:54:01 -0300

    Initial repo

---
+++ efeitos.js
@@ -0,0 +1,10 @@
+function toggle(circulo) {
+    var fill   = circulo.style.fill;
+    var stroke = circulo.style.fill;
+    
+    if (fill = 'red') {
+        fill = 'blue';
+    } else {
+        fill = 'red';
+    }
+}
